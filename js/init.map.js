aa840df591d8f2a6c40ec6ff855024551a9ed441
egor-podolskij <egorpodolskij10@gmail.com>
2017-07-24 10:15:24 +0300

    Resolve #28

--- js/init.map.js
+++ js/init.map.js
@@ -49,7 +49,7 @@ if(loaded_points.status == true ){
     // load_points.data.points
     // forEach метод работы с массивами, пробежка по каждому элементу
     // function(point) выполняет действие с передачей load_points.data.points в переменную point
-    load_points.data.point.forEach(function(point){
+    loaded_points.data.point.forEach(function(point){
         
         // create_marker вызов ранее объявленой функции
         // map первый аргумент функции, подставляем значение переменной map
